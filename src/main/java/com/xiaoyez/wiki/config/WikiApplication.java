e79cdff2c245d47a6af9c479666d0bd3bf6adac8
ZXXYy <308169188@qq.com>
2021-04-11 14:55:29 +0800

    2-6 开发Hello World接口 访问地址：http://127.0.0.1:8880/hello

--- src/main/java/com/xiaoyez/wiki/config/WikiApplication.java
+++ src/main/java/com/xiaoyez/wiki/config/WikiApplication.java
@@ -1,12 +1,16 @@
-package com.xiaoyez.wiki;
+package com.xiaoyez.wiki.config;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.core.env.Environment;
 
+// @ComponentScan({"com.xiaoyez", "com.test"}) 扫描包
+@ComponentScan({"com.xiaoyez"})
 @SpringBootApplication
+
 public class WikiApplication {
 
     private static final Logger LOG = LoggerFactory.getLogger(WikiApplication.class);
