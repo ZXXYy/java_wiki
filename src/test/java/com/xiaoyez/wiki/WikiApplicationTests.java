1b5d95c69d86bd1b6b92e9adc698b6fda29212b6
ZXXYy <308169188@qq.com>
2021-04-11 13:55:40 +0800

    2-2 init

---
+++ src/test/java/com/xiaoyez/wiki/WikiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xiaoyez.wiki;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WikiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
