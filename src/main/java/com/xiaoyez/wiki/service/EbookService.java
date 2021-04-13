c61b91a89915844b05ad49b54bc629f379bf1cb6
ZXXYy <308169188@qq.com>
2021-04-13 10:57:23 +0800

    3-7 封装请求参数和返回参数

--- src/main/java/com/xiaoyez/wiki/service/EbookService.java
+++ src/main/java/com/xiaoyez/wiki/service/EbookService.java
@@ -1,10 +1,15 @@
 package com.xiaoyez.wiki.service;
 
 import com.xiaoyez.wiki.domain.Ebook;
+import com.xiaoyez.wiki.domain.EbookExample;
 import com.xiaoyez.wiki.mapper.EbookMapper;
+import com.xiaoyez.wiki.req.EbookReq;
+import com.xiaoyez.wiki.resp.EbookResp;
+import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -13,8 +18,19 @@ public class EbookService {
     @Resource
     private EbookMapper ebookMapper;
 
-    public List<Ebook> list(){
-        return ebookMapper.selectByExample(null);
+    public List<EbookResp> list(EbookReq req){
+        EbookExample ebookExample = new EbookExample();
+        EbookExample.Criteria criteria = ebookExample.createCriteria();
+        criteria.andNameLike("%"+req.getName()+"%");
+        List<Ebook> ebookList = ebookMapper.selectByExample(ebookExample);
+
+        List<EbookResp> respList = new ArrayList<>();
+        for (Ebook ebook : ebookList) {
+            EbookResp ebookResp = new EbookResp();
+            BeanUtils.copyProperties(ebook, ebookResp);
+            respList.add(ebookResp);
+        }
+        return respList;
     }
 
 }
