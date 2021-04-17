f7f14a71666efb993584f1a48c0931d54f1079c8
ZXXYy <308169188@qq.com>
2021-04-17 16:29:33 +0800

    5-4 列表查询接口支持动态SQL name参数有值时按name查，没值时查全部 封面小图样式调整

--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -109,7 +109,7 @@ export default defineComponent({
 
       onMounted(() => {
           console.log("onMounted");
-          axios.get("http://127.0.0.1:8880/ebook/list?name=Spring").then(
+          axios.get("http://127.0.0.1:8880/ebook/list").then(
           (response) => {
               const data = response.data;
               ebooks.value = data.content;
@@ -138,3 +138,14 @@ export default defineComponent({
 
 });
 </script>
+
+<style scoped>
+    .ant-avatar{
+        width: 50px;
+        height: 50px;
+        line-height: 50px;
+        border-radius: 8%;
+        margin: 5px 0;
+    }
+
+</style>
